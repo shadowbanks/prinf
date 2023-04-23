6f05a3d823219fa0f9f5270275ac7fa6b77fbb72
Williams <akanniwilliams@gmail.com>
2023-04-23 06:42:31 +0100

    'Create project layout'

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define BUFFSIZE 1024;
+#define INT_BUFF 20;
+
+int _printf(const char, ...);
+
+#endif /*MAIN_H*/
---
+++ printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - Implementation of the printf function
+ * @format: input string
+ *
+ * Return: number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+}
