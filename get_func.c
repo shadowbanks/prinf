35d584dde06a4481773db1ed5dc34e5abac918f9
Droid <44108804+Effah001@users.noreply.github.com>
2023-04-25 07:29:04 +0000

    Update get_func.c

--- get_func.c
+++ get_func.c
@@ -23,7 +23,7 @@ int (*get_func(char specifier))(char *, va_list)
 		{'x', x_func},
 		{'X', X_func},
 		{'p', ptr_func},
-		{'S', String_func},
+		{'S', string_func},
 		{'r', revs_string},
 		{'R', rot13_func},
 		{'\0', NULL}
